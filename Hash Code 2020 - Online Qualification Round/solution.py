8f8c261eac960705e93f5d22fbc0843e12187c25
lamonkey <88therisingsun@gmail.com>
2021-02-21 21:20:12 -0500

    push code

---
+++ Hash Code 2020 - Online Qualification Round/solution.py
@@ -0,0 +1,4 @@
+class solution:
+    with open("/Users/lamonkey/Desktop/hashcode/Hash\ Code\ 2020\ -\ Online\ Qualification\ Round/a_example.txt",'r') as file:
+        for line in file:
+            print(line)
